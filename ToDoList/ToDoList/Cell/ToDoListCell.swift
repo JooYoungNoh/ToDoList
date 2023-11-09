2a8bd6c22b15d8523e8b5b0c2f43837792ac869c
NohJooYoung <nohjoo97@naver.com>
2023-11-09 23:34:12 +0900

    [add] 리스트 임시 구현

---
+++ ToDoList/ToDoList/Cell/ToDoListCell.swift
@@ -0,0 +1,100 @@
+//
+//  ToDoListCell.swift
+//  ToDoList
+//
+//  Created by 노주영 on 2023/11/09.
+//
+
+import UIKit
+
+class ToDoListCell: UITableViewCell {
+    static let identifier = "ToDoListCell"
+    
+    let stackView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray6
+        return view
+    }()
+    
+    let title: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "NotoSansKR-Bold", size: 20)
+        label.textColor = .black
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    let descriptionData: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "NotoSansKR-Regular", size: 12)
+        label.textColor = .black
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    let completedImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.isHidden = true
+        return imageView
+    }()
+       
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(self.stackView)
+        
+        self.stackView.addSubview(completedImage)
+        self.stackView.addSubview(title)
+        self.stackView.addSubview(descriptionData)
+        
+        self.stackView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        self.completedImage.snp.makeConstraints { make in
+            make.top.equalTo(stackView.snp.top).offset(10)
+            make.trailing.equalTo(stackView.snp.trailing).offset(-20)
+            make.bottom.equalTo(stackView.snp.bottom).offset(-10)
+            make.width.equalTo(50)
+        }
+        
+        self.title.snp.makeConstraints { make in
+            make.top.equalTo(stackView.snp.top).offset(10)
+            make.leading.equalTo(stackView.snp.leading).offset(20)
+            make.trailing.equalTo(completedImage.snp.trailing).offset(-10)
+            make.height.equalTo(30)
+        }
+        
+        self.descriptionData.snp.makeConstraints { make in
+            make.top.equalTo(title.snp.bottom).offset(5)
+            make.bottom.equalTo(stackView.snp.bottom).offset(-10)
+            make.leading.trailing.equalTo(title)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        self.title.text = ""
+        self.descriptionData.text = ""
+        self.completedImage.image = nil
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ToDoList/ToDoList/View/ToDoListView.swift
+++ ToDoList/ToDoList/View/ToDoListView.swift
@@ -6,11 +6,18 @@
 //
 
 import UIKit
+import SnapKit
 
 class ToDoListView: UIViewController {
 
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.register(ToDoListCell.self, forCellReuseIdentifier: ToDoListCell.identifier)
+        
         self.setView()
     }
     
@@ -22,17 +29,41 @@ class ToDoListView: UIViewController {
         self.navigationItem.title = "ToDoList"
         self.navigationController?.navigationBar.prefersLargeTitles = true
         
+        //테이블 뷰
+        self.view.addSubview(self.tableView)
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+        
+    }
+}
+
+extension ToDoListView: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ToDoListCell.identifier, for: indexPath) as? ToDoListCell else { return UITableViewCell() }
+        
+        cell.title.text = "실험중"
+        cell.descriptionData.text = "아아아아아ㅏ앙아앙아앙아아아아앙"
+        cell.completedImage.image = UIImage(systemName: "checkmark.circle.fill")
+        
+        return cell
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! ToDoListCell
+        if cell.completedImage.isHidden {
+            cell.completedImage.isHidden = false
+        } else {
+            cell.completedImage.isHidden = true
+        }
+    }
 }
